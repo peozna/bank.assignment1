520b49ae56b63cf5a2d8aff0c5232d18dd24dd39
Nathalie <nannaskrutt@gmail.com>
2026-01-27 14:05:33 +0100

    finished customer class

--- src/nattrn2/Customer.java
+++ src/nattrn2/Customer.java
@@ -2,13 +2,30 @@ package nattrn2;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Customer.java
+ *
+ * Representerar en kund med förnamn, efternamn, personnummer och en lista med tillhörande kontonummer.
+ * Klassen hanterar ändring av namn och presentation av kundinfo.
+ * Logik:
+ * Den innehåller endast information om kunden ingen logik för konton eller transaktioner.
+ *
+ * @author Nathalie Törnkvist
+ * Användarnamn: nattrn-2
+ * */
+
 public class Customer {
     private String firstName;
     private String lastName;
     private int personalNumber;
     private List<Integer> accountIds;
 
-    public Customer (String firstName, String lastName, int personalNumber, List<Integer>accountIds ) {
+    /**
+     * Konstruktor för customer
+     * Skapar en kund utan konton
+     * Konton skapas och kopplas till kunden senare av BankLogic
+     * */
+    public Customer (String firstName, String lastName, int personalNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.personalNumber = personalNumber;
@@ -31,14 +48,21 @@ public class Customer {
         return accountIds;
     }
 
+    //Ändrar kundens förnamn
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    //Ändrar kundens efternamn
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Returnerar en sträng med kundinformation.
+     * Innehåller namn, personnummer och kontonummer.
+     *
+     * */
     public String getCustomerInfo() {
         return firstName + " " + lastName + " " + String.valueOf(getPersonalNumber()) + " " + getAccountIds().toString();
     }
