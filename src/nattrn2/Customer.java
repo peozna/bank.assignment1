900d4fa8ee6a998f4b57b69232929b16f5d6a24d
Nathalie <nannaskrutt@gmail.com>
2026-01-27 13:42:31 +0100

    Added customer constructor, getters and setters for name

--- src/nattrn2/Customer.java
+++ src/nattrn2/Customer.java
@@ -1,4 +1,33 @@
 package nattrn2;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Customer {
+    private String firstName;
+    private String lastName;
+    private int personalNumber;
+    private List<Integer> accountIds;
+
+    public Customer (String firstName, String lastName, int personalNumber, List<Integer>accountIds ) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.personalNumber = personalNumber;
+        this.accountIds = new ArrayList<>();
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
