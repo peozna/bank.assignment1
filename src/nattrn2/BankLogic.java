1abc23534b1a2ee8ec8d69c4560c4e57e7b46b28
Nathalie <nannaskrutt@gmail.com>
2026-01-28 14:50:03 +0100

    added method getAllCustomers

--- src/nattrn2/BankLogic.java
+++ src/nattrn2/BankLogic.java
@@ -1,4 +1,31 @@
 package nattrn2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BankLogic {
+    List<Customer> customers = new ArrayList<>();
+
+    static void main() {
+
+    }
+    /**
+     * Metoden returnerar en lista med strängar som representeras bankens kunder.
+     * Om inga kunder finns returneras en tom lista.
+     * */
+    public List<String> getAllCustomers() {
+        List<String> customerString = new ArrayList<>();;
+        if (customers.isEmpty()) {
+            return customerString;
+        }
+
+        for (Customer customer : customers) {
+            String addCustomer = String.valueOf(customer.getPersonalNumber()) + " " +
+            customer.getFirstName() + " " +
+            customer.getLastName();
+
+            customerString.add(addCustomer);
+        }
+        return customerString;
+    }
 }
